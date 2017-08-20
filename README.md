60be40565b0b425e1470a9d6cc6cf0c45dcfabcb
Tim-Ferguson <trfergus@asu.edu>
2017-08-20 12:43:45 -0700

    Initial

---
+++ README.md
@@ -0,0 +1,2 @@
+# brew-map
+Server Code for the Brew-map app
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}))
+var port = process.env.PORT || 8080;
+MongoClient.connect(process.env.mlaburl , function(err, database){
+
+	if(err) return console.log(err);
+
+	db = database
+
+	app.listen(port, function(){
+		console.log('listening on '+port);
+	})
+	app.get('*',function(req,res,next){ 
+		if(req.headers['x-forwarded-proto']!='https') 
+			res.redirect('https://find-rr-beer.herokuapp.com'+req.url) ;
+		else 
+			next();
+	})
+	app.get('/', function(req, res) {
+		res.sendFile(__dirname + '/index.html');
+	})
+
+	app.get('/Locations', function(req, res) {
+		console.log("hello");
+		db.collection('BeerLocations').find().toArray(function(err, results){
+			res.send(results);
+		})
+	})
+
+	app.post('/AddLocation', function(req, res){
+		//console.log(req.body);
+	})  
+})
+
+
+
+

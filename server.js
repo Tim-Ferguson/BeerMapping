d2c631562ebb1ad849be81b7ff74aa21a38cb629
Tim-Ferguson <trfergus@asu.edu>
2017-08-20 12:49:26 -0700

    change redirect :)

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ MongoClient.connect(process.env.mlaburl , function(err, database){
 	})
 	app.get('*',function(req,res,next){ 
 		if(req.headers['x-forwarded-proto']!='https') 
-			res.redirect('https://find-rr-beer.herokuapp.com'+req.url) ;
+			res.redirect('https://brew-maps.herokuapp.com'+req.url) ;
 		else 
 			next();
 	})
